e22c8e291dc9b354c91393299acbbd96822ecf7d
Sushil <sushilt553@gmail.com>
2020-06-08 15:47:43 -0400

    Status messages

---
+++ script1.js
@@ -0,0 +1,25 @@
+let currentPlayer = "X";
+let statusDisplay = `It's ${currentPlayer}'s turn`;
+
+const winningMessage = () => {
+    statusDisplay = `Congrats, ${currentPlayer} won the game.`
+}
+
+const drawMessage = () => {
+    statusDisplay = `The game is draw.`
+}
+
+const state = ["", "", "", "", "", "", "", "", ""]
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 5]
+]
+
+const 
